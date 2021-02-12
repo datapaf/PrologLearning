20bc1cbdf200e620d2df4901c95dd8faf014a7a5
datapaf <georgyandryuschenko@gmail.com>
2021-02-12 18:50:40 +0300

    Variables.pl

---
+++ Variables.pl
@@ -0,0 +1,10 @@
+what_grade(5) :- 
+    write('go to kindergarten').
+
+what_grade(6) :-
+    write('go to first grade').
+
+what_grade(Other) :-
+    Grade is Ohter - 5,
+    format('go to grade ~w', [Grade]).
+    
